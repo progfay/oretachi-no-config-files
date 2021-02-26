6e0a87cdec231e88d2807241bfb89b6bce01ada7
mikiya771 <22876341+mikiya771@users.noreply.github.com>
2021-02-26 09:26:38 +0900

    fix: application部分の名前変更

--- scripts/app-language-change.sh
+++ scripts/app-language-change.sh
@@ -5,8 +5,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd ${SCRIPT_DIR}
 source env.sh
 
-sudo systemctl stop ${DISABLE_APPLICATION_SERVICE}
-sudo systemctl disable ${DISABLE_APPLICATION_SERVICE}
-
 sudo systemctl enable ${DEFAULT_APPLICATION_SERVICE}
 sudo systemctl start ${DEFAULT_APPLICATION_SERVICE}
--- scripts/env.sh
+++ scripts/env.sh
@@ -5,5 +5,5 @@ export MYSQL_CONF_PATH="/etc/mysql/mysql.conf.d/mysqld.cnf"
 export ACCESS_LOG="/var/log/nginx/access.log"
 export MYSQL_SLOW_LOG="/tmp/slow.log"
 
-export DEFAULT_APPLICATION_SERVICE="r-isucon-go.service"
+export DEFAULT_APPLICATION_SERVICE="niita-go.service"
 export DISABLE_APPLICATION_SERVICE="r-isucon-node.service"
